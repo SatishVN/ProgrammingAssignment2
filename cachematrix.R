31bd90381ba6bd131bb30ff211a733ff810fc19d
SatishVN <mailsatisha@gmail.com>
2016-07-25 18:26:58 +0530

    Committed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and the cacheSolve environment exhibit caching in a different 
+## environment
 
-## Write a short comment describing this function
+## makeCacheMatrix creates the relevant get and set functions for returning matrix
+## and the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) {
+    i <<- solve
+    print("in setting")
+  }
+  getsolve <- function() i
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function checks if the the inverse already exists and returns the same
+## if exists, else creates inverse and returns
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getsolve()
+  if(!is.null(i)) {
+    print("getting cached matrix")
+    return(i)
+  }
+  print("getting new inverse")
+  data <- x$get()
+  print(data)
+  i <- solve (data, ...)
+  x$setsolve(i)
+  i
 }
